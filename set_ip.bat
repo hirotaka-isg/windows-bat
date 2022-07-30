ad006e36830e4aabd57185d9872a88aa1e656ec4
hirotaka-isg <hirotaka-isg@hotmail.com>
2022-07-30 13:56:13 +0900

    set_ipの修正
    汎用的な使い方ができるように修正

--- set_ip.bat
+++ set_ip.bat
@@ -1,39 +1,39 @@
 @echo off
-rem IP設定
-rem 管理者で実行
 
-echo 10
-echo 20
-echo 30
+rem 目的：拠点に合わせてネットワークの設定を切り替える
+rem 作成日：2022/07/30
+rem 管理者で実行して下さい
 
-echo 拠点を半角数字で入力してください
+echo 10：拠点名１
+echo 20：拠点名２
+echo 30：拠点名３
+echo 上記から拠点を半角数字で入力してください
 set /p location=
 
-IF %location%==10 goto 10
-IF %location%==20 goto 20
-IF %location% goto 30
+rem 各ロケーションに合わせて分岐
+IF %location%==10 goto location_10
+IF %location%==20 goto location_20
+IF %location%==30 goto location_30
 
-rem 設定情報
-
-:10
-echo パターン１
+:location_10
+echo 拠点１
+echo 設定情報
 set interface_name="Wi-Fi"
-set ip_address=192.168.0.10
-set subnet_mask=255.255.255.0
-set default_gateway=192.168.0.1
+set ip_address=XXX.XXX.XXX.XXX
+set subnet_mask=XXX.XXX.XXX.XXX
+set default_gateway=XXX.XXX.XXX.XXX
 
 rem DNSサーバー情報
-set dns_server1=192.168.0.250
-set dns_server2=192.168.0.251
+set dns_server1=XXX.XXX.XXX.XXX
+set dns_server2=XXX.XXX.XXX.XXX
 
 echo interface_name="Wi-Fi"
-echo ip_address=192.168.0.10
-echo subnet_mask=255.255.255.0
-echo default_gateway=192.168.0.1
-echo dns_server1=192.168.0.250
-echo dns_server2=192.168.0.251
+echo ip_address=XXX.XXX.XXX.XXX
+echo subnet_mask=XXX.XXX.XXX.XXX
+echo default_gateway=XXX.XXX.XXX.XXX
+echo dns_server1=XXX.XXX.XXX.XXX
+echo dns_server2=XXX.XXX.XXX.XXX
 
-rem netsh interface ip set address "(アダプタ名)" static (IPアドレス) (ネットマスク) (デフォルトゲートウェイ)
 netsh interface ip set address %interface_name% static %ip_address% %subnet_mask% %default_gateway%
 
 rem DNSサーバの設定
@@ -42,14 +42,40 @@ netsh interface ipv4 add dns name="%interface_name%" addr="%dns_server2%" index=
 pause
 goto :EOF
 
-:20
-echo パターン２DHCP
+:location_20
+echo 拠点２
+echo 設定情報
 set interface_name="Wi-Fi"
-rem IP アドレスを DHCP に設定する
+set ip_address=XXX.XXX.XXX.XXX
+set subnet_mask=XXX.XXX.XXX.XXX
+set default_gateway=XXX.XXX.XXX.XXX
+
+rem DNSサーバー情報
+set dns_server1=XXX.XXX.XXX.XXX
+set dns_server2=XXX.XXX.XXX.XXX
+
+echo interface_name="Wi-Fi"
+echo ip_address=XXX.XXX.XXX.XXX
+echo subnet_mask=XXX.XXX.XXX.XXX
+echo default_gateway=XXX.XXX.XXX.XXX
+echo dns_server1=XXX.XXX.XXX.XXX
+echo dns_server2=XXX.XXX.XXX.XXX
+
+netsh interface ip set address %interface_name% static %ip_address% %subnet_mask% %default_gateway%
+
+rem DNSサーバの設定
+netsh interface ipv4 set dns name="%interface_name%" source=static addr="%dns_server1%" register=non validate=no
+netsh interface ipv4 add dns name="%interface_name%" addr="%dns_server2%" index=2 validate=no
+
+pause
+goto :EOF
+
+:location_30
+echo 拠点３（DHCP）
+set interface_name="Wi-Fi"
+rem DHCPに設定
 netsh interface ipv4 set address name=%interface_name% source=dhcp
-rem DNS サーバーの設定を DHCP に設定します。
 netsh interface ipv4 set dnsservers name=%interface_name% source=dhcp
 pause
 goto :EOF
 
-echo 終了
